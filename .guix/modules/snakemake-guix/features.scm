6eff85c6355dc721a7e7cc7b7ef134836e87b575
Nicolas Graves <ngraves@ngraves.fr>
2026-05-16 23:25:34 +0200

    channel: Add snakemake feature

---
+++ .guix/modules/snakemake-guix/features.scm
@@ -0,0 +1,39 @@
+;; Copyright © 2026 Nicolas Graves <ngraves@ngraves.fr>
+
+(define-module (snakemake-guix features)
+  #:autoload (rde predicates) (ensure-pred)
+  #:autoload (rde features) (feature)
+  #:use-module (gnu services)
+  #:use-module (gnu home services)
+  #:use-module (gnu packages emacs-xyz)
+  #:use-module (guix gexp)
+  #:use-module (snakemake-guix packages)
+  #:export (feature-snakemake))
+
+(define* (feature-snakemake
+          #:key
+          (snakemake snakemake-with-software-deployment)
+          (emacs-snakemake-mode emacs-snakemake-mode)
+          (snakemake-plugins (list python-snakemake-storage-plugin-http)))
+  "Configure and set up tooling for Snakemake."
+  (ensure-pred file-like? snakemake)
+  (ensure-pred file-like? emacs-snakemake-mode)
+  (ensure-pred list-of-file-like? snakemake)
+
+  (define f-name 'snakemake)
+
+  (define (get-home-services config)
+    "Return home services related to Snakemake."
+    (append
+     (list
+      (simple-service 'add-snakemake-home-packages home-profile-service-type
+        (append (list snakemake)
+                snakemake-plugins
+                (if (get-value 'emacs config #f)
+                    (list emacs-snakemake-mode)
+                    (list)))))))
+
+  (feature
+   (name f-name)
+   (values `((,f-name . ,snakemake)))
+   (home-services-getter get-home-services)))
