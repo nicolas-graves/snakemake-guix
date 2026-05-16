c16b16331f7806aad7150a42f59930b4a452d817
Nicolas Graves <ngraves@ngraves.fr>
2026-05-16 23:19:11 +0200

    channel: Add emacs-snakemake-mode

--- .guix/modules/snakemake-guix/packages.scm
+++ .guix/modules/snakemake-guix/packages.scm
@@ -1,6 +1,7 @@
 ;; Copyright © 2025, 2026 Nicolas Graves <ngraves@ngraves.fr>
 
 (define-module (snakemake-guix packages)
+  #:use-module (guix build-system emacs)
   #:use-module (guix build-system pyproject)
   #:use-module (guix build-system python)
   #:use-module (guix diagnostics)
@@ -13,6 +14,7 @@
   #:use-module ((guix utils) #:select (substitute-keyword-arguments))
   #:use-module (gnu packages)
   #:use-module (gnu packages check)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages package-management)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-build)
@@ -58,6 +60,38 @@
 
 (define-syntax-rule (snakemake-guix-patches file-name ...)
   (list (search-snakemake-guix-patch file-name) ...))
+
+(define-public emacs-snakemake-mode
+  (let ((commit "e4751a951a53c4d4610b2eb17469a21177cab6bc")
+        (revision "0"))
+    (package
+      (name "emacs-snakemake-mode")
+      (version (git-version "2.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+                (url "https://git.kyleam.com/snakemake-mode")
+                (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0b19bfk2d29v6ckh0sxyrrl8mzqqpmnxbs9rp58rf7ipk4rp6xwl"))))
+      (build-system emacs-build-system)
+      (arguments
+       (list
+        ;; XXX: Tests involving the snakemake binary fail.
+        #:tests? #f
+        #:test-command #~(list "make" "test")))
+      (native-inputs (list guix:snakemake))
+      (propagated-inputs (list emacs-transient))
+      (home-page "https://git.kyleam.com/snakemake-mode")
+      (synopsis "Major mode for editing Snakemake files")
+      (description
+       "This package provides support for editing Snakemake files in Emacs.  It
+builds on Python mode to provide fontification, indentation, and imenu indexing
+for Snakemake's rule blocks, as well as an interface for running Snakemake
+commands and support for highlighting embedded R code.")
+      (license license:gpl3+))))
 
 (define-public python-udocker
   (package
